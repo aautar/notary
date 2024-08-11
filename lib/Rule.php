0437f131925ba90d4e454933823e3ddd9cde1a0e
Avishkar Autar <aautar@fragmentspace.com>
2024-08-10 23:49:48 -0400

    add phpdocs, return type declarations on Rule class members

--- lib/Rule.php
+++ lib/Rule.php
@@ -4,28 +4,44 @@ namespace Notary;
 
 class Rule
 {
+    /**
+     * @var string
+     */
     protected $id;
+
+    /**
+     * @var string
+     */
     protected $ruleCheckFailureMessage;
+
+    /**
+     * @var callable
+     */
     protected $ruleCheckFunction;
 
-    public function __construct($_id, $_ruleCheckFailureMessage, $_ruleCheckFunction)
+    /**
+     * @param string $_id Identifier for this rule
+     * @param string $_ruleCheckFailureMessage Message to surface when validation rule fails
+     * @param callable $_ruleCheckFunction Function to evaluate validation rule
+     */
+    public function __construct(string $_id, string $_ruleCheckFailureMessage, callable $_ruleCheckFunction)
     {
         $this->id = $_id;
         $this->ruleCheckFailureMessage = $_ruleCheckFailureMessage;
         $this->ruleCheckFunction = $_ruleCheckFunction;
     }
 
-    function getId()
+    function getId(): string
     {
         return $this->id;
     }
 
-    function getRuleCheckFailureMessage()
+    function getRuleCheckFailureMessage(): string
     {
         return $this->ruleCheckFailureMessage;
     }
 
-    function getRuleCheckFunction()
+    function getRuleCheckFunction(): callable
     {
         return $this->ruleCheckFunction;
     }
